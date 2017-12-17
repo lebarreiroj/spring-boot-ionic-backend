cf4d6f7a96460d5d4d0ca9bfb2d95d0be8d90c18
Luis Eduardo <lebarreiroj@gmail.com>
2017-12-16 22:09:32 -0200

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/lebarreiroj/cursomc/domain/Cliente.java
+++ src/main/java/com/lebarreiroj/cursomc/domain/Cliente.java
@@ -6,10 +6,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import com.lebarreiroj.cursomc.domain.enums.TipoCliente;
 
@@ -26,8 +29,11 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
+	@ElementCollection
+	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
 	public Cliente() {
