ff830021e5f325aac68f9b7385e5807ac0fb2ba4
Luis Eduardo <lebarreiroj@gmail.com>
2018-04-01 20:03:28 -0300

    Salvando perfis de usuários na base de dados

--- src/main/java/com/lebarreiroj/cursomc/services/DBService.java
+++ src/main/java/com/lebarreiroj/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.lebarreiroj.cursomc.domain.PagamentoComCartao;
 import com.lebarreiroj.cursomc.domain.Pedido;
 import com.lebarreiroj.cursomc.domain.Produto;
 import com.lebarreiroj.cursomc.domain.enums.EstadoPagamento;
+import com.lebarreiroj.cursomc.domain.enums.Perfil;
 import com.lebarreiroj.cursomc.domain.enums.TipoCliente;
 import com.lebarreiroj.cursomc.repositories.CategoriaRepository;
 import com.lebarreiroj.cursomc.repositories.CidadeRepository;
@@ -123,16 +124,21 @@ public class DBService {
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null,"Maria Silva", "lebarreiroj@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null,"Ana Costa", "luis.lebj@gmail.com",   "91335261338", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli1.getTelefones().addAll(Arrays.asList("34344343", "32133123"));
+		cli2.addPerfil(Perfil.ADMIN);
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avanida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avanida Floriano", "2106", null, "Centro", "70702000", cli2, c3);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli1.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.save(Arrays.asList(cli1));
-		enderecoRepository.save(Arrays.asList(e1, e2));
+		clienteRepository.save(Arrays.asList(cli1, cli2));
+		enderecoRepository.save(Arrays.asList(e1, e2, e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
